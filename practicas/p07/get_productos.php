6a76e0e600f1916d0cf28eac55dad3b0e487fc03
GabrieCV2003 <gabrielcorona.buap@gmial.com>
2025-02-12 15:04:12 -0600

    Se modificio el documento get_productos.php, tal ves aparece editado todo por que se estaba editando una copia del codigo para hacer pruebas y al final solo copie y pegue el funcional

--- practicas/p07/get_productos.php
+++ practicas/p07/get_productos.php
@@ -1,49 +1,75 @@
 <?php
-    header("Content-Type: application/json; charset=utf-8"); 
+    header("Content-Type: application/xhtml+xml; charset=utf-8");
+    
+    if (!isset($_GET['tope'])) {
+        die('<p>Parámetro "tope" no detectado...</p>');
+    }
+    
+    $tope = intval($_GET['tope']); // Convertir a entero para seguridad
     $data = array();
-
-	if(isset($_GET['tope']))
-    {
-		$tope = $_GET['tope'];
+    
+    /** SE CREA EL OBJETO DE CONEXION */
+    @$link = new mysqli('localhost', 'root', 'vallelly2003', 'marketzone');
+    
+    /** Comprobar la conexión */
+    if ($link->connect_errno) {
+        die('<p>Falló la conexión: ' . htmlspecialchars($link->connect_error) . '</p>');
     }
-    else
-    {
-        die('Parámetro "tope" no detectado...');
+    
+    /** Consultar productos con unidades menores o iguales al tope */
+    if ($result = $link->query("SELECT * FROM productos WHERE unidades <= $tope")) {
+        $data = $result->fetch_all(MYSQLI_ASSOC);
+        $result->free();
     }
-
-	if (!empty($tope))
-	{
-		/** SE CREA EL OBJETO DE CONEXION */
-		@$link = new mysqli('localhost', 'root', 'vallelly2003', 'marketzone');
-        /** NOTA: con @ se suprime el Warning para gestionar el error por medio de código */
-
-		/** comprobar la conexión */
-		if ($link->connect_errno) 
-		{
-			die('Falló la conexión: '.$link->connect_error.'<br/>');
-			//exit();
-		}
-
-		/** Crear una tabla que no devuelve un conjunto de resultados */
-		if ( $result = $link->query("SELECT * FROM productos WHERE unidades <= $tope") ) 
-		{
-            /** Se extraen las tuplas obtenidas de la consulta */
-			$row = $result->fetch_all(MYSQLI_ASSOC);
-
-            /** Se crea un arreglo con la estructura deseada */
-            foreach($row as $num => $registro) {            // Se recorren tuplas
-                foreach($registro as $key => $value) {      // Se recorren campos
-                    $data[$num][$key] = utf8_encode($value);
-                }
-            }
-
-			/** útil para liberar memoria asociada a un resultado con demasiada información */
-			$result->free();
-		}
-
-		$link->close();
-
-        /** Se devuelven los datos en formato JSON */
-        echo json_encode($data, JSON_PRETTY_PRINT);
-	}
-	?>
+    
+    $link->close();
+    
+    /* Generar XHTML */
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Lista de Productos</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+</head>
+<body>
+    <h3>PRODUCTOS</h3>
+    <br/>
+    <?php if (!empty($data)): ?>
+        <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Marca</th>
+                    <th scope="col">Modelo</th>
+                    <th scope="col">Precio</th>
+                    <th scope="col">Unidades</th>
+                    <th scope="col">Detalles</th>
+                    <th scope="col">Imagen</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($data as $producto): ?>
+                    <tr>
+                        <th scope="row"><?php echo htmlspecialchars($producto['id']); ?></th>
+                        <td><?php echo htmlspecialchars($producto['nombre']); ?></td>
+                        <td><?php echo htmlspecialchars($producto['marca']); ?></td>
+                        <td><?php echo htmlspecialchars($producto['modelo']); ?></td>
+                        <td><?php echo htmlspecialchars($producto['precio']); ?></td>
+                        <td><?php echo htmlspecialchars($producto['unidades']); ?></td>
+                        <td><?php echo utf8_encode($producto['detalles']); ?></td>
+                        <td><img src="<?php echo htmlspecialchars($producto['imagen']); ?>" alt="Imagen del producto" /></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php else: ?>
+        <script>
+            alert('No hay productos que cumplan con el criterio.');
+        </script>
+    <?php endif; ?>
+</body>
+</html>
