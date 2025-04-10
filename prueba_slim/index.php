0c4b48b7d2f920f9e72e49f254aa00bf1db387ba
GabrielCV2003 <gabrielcorona.buap@gmail.com>
2025-04-10 13:12:11 -0600

    Se realizo el segundo ejemplo del video, pasando datos por parametro mediante la URL

--- prueba_slim/index.php
+++ prueba_slim/index.php
@@ -15,4 +15,9 @@ $app->get('/', function ($request, $response, $args) {
     return $response;
 });
 
+$app->get("/hola[/{nombre}]", function( $request, $response, $args ){
+    $nombre = $args["nombre"] ?? "invitado";
+    $response->getBody()->write("Hola, " . $nombre);
+    return $response;
+});
 $app->run();
