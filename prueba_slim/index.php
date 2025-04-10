a202d7c32a1111b47ce104231959bc57dae27e67
GabrielCV2003 <gabrielcorona.buap@gmail.com>
2025-04-10 11:28:14 -0600

    Se creo la carpeta de la practica, y a su vez se realizo el primer ejercicio a manera de prueba

---
+++ prueba_slim/index.php
@@ -0,0 +1,18 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require 'vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Esta línea debe coincidir con la carpeta donde está tu index.php:
+$app->setBasePath("/tecweb/prueba_slim");
+
+$app->get('/', function ($request, $response, $args) {
+    $response->getBody()->write("Hola mundo Slim!!!");
+    return $response;
+});
+
+$app->run();
